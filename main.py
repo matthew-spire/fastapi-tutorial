220ca70abeb1644669457526e79fb552f4987348
Matthew Spire <matthew.t.spire@gmail.com>
2025-02-16 12:16:31 -0500

    Part three query parameters

--- main.py
+++ main.py
@@ -15,13 +15,28 @@ async def post():
 async def put():
     return {"message": "This is a PUT request."}
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Fiz"}]
+
+# Query parameters - Next two routes
 @app.get("/items")
-async def list_items():
-    return {"message": "This is a list of items."}
+async def list_items(skip: int = 0, limit: int = 10):
+    # return {"message": "This is a list of items."}
+    return fake_items_db[skip : skip + limit]
 
 @app.get("/items/{item_id}")
-async def get_item(item_id: int):
-    return {"item_id": item_id}
+async def get_item(item_id: int, sample_query_param: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        # return {"item_id": item_id, "q": q}
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum odor amet, consectetuer adipiscing elit."
+            }
+        )
+    # return {"item_id": item_id}
+    return item
 
 @app.get("/users")
 async def list_users():
@@ -34,6 +49,20 @@ async def get_current_user():
 @app.get("/users/{user_id}")
 async def get_user(user_id: int):
     return {"user_id": user_id}
+
+# Multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(user_id: int, item_id: int, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum odor amet, consectetuer adipiscing elit."
+            }
+        )
+    return item
 
 class FoodEnum(str, Enum):
     fruits = "Fruits"
